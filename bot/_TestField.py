ddb56595ac6ec26a9077144874101f1a8d52dff6
hazekezia <hazekezia@gmail.com>
2022-08-28 10:10:25 +0700

    Hide TestField command

--- bot/_TestField.py
+++ bot/_TestField.py
@@ -4,13 +4,13 @@ class TestField(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
-    @commands.command(brief="TestField")
+    @commands.command(brief="TestField", hidden=True)
     async def testo(self, ctx):
         await ctx.message.delete()
         await ctx.send("deleted")
         return
     
-    @commands.command(brief="TestField")
+    @commands.command(brief="TestField", hidden=True)
     async def blough(self, ctx):
         await ctx.send("lu gobloug")
         return
